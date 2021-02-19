82b8fa67dc794ebac2bae5298c24d9958cec60db
bassem.chdid <b.chdid1992@gmail.com>
2021-02-19 15:50:44 +0200

    revamp

--- src/screens/functionComponent/FirstFunctionComponent.js
+++ src/screens/functionComponent/FirstFunctionComponent.js
@@ -8,40 +8,15 @@ const FirstFunctionComponent = () => {
     let [randomNumber, setRandomNumber] = useState(0);
     let [showComponentDetail, setShowComponentDetail] = useState(false);
 
-    const fetchArticles = () => {
-        try {
-            const API_KEY = 'CDSNAiFyxiaylxDzfjLVc3VCkoAjJPLA';
-            axios(`https://api.nytimes.com/svc/mostpopular/v2/viewed/1.json?api-key=${API_KEY}`)
-                .then( res =>{
-                    console.log("res",res.data )
-                }
-                )
-                .catch( error =>{
-                        console.log("error",error )
-                    }
-                )
-        } catch (error) {
-            console.log("error",error )
-        }
-    }
-
     // Like componentDidMount
     useEffect(() => {
         setCount(10);
-        // fetchArticles()
-        //     .then(result =>{
-        //         console.log("result",result )
-        //     })
-        //     .catch(error =>{
-        //         console.log("error",error )
-        //     })
     }, [])
 
     // Like componentDidUpdate
     useEffect(() => {
         // Execute this block of code when count changes
         setRandomNumber(Math.random() * 10);
-        fetchArticles()
     }, [count])
 
     // Like componentWillUnmount
